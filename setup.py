2a3442f135dddb7e2f387aa8666f6df1543b9cf7
RNLFoof <z.zablotsky@gmail.com>
2022-05-14 14:02:49 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='ZachsStupidImageLibrary',
+   version='1.0',
+   description='Beware',
+   license="MIT",
+   long_description=long_description,
+   author='Zachary Zablotsky',
+   url="https://github.com/RNLFoof/ZachsStupidImageLibrary",
+   packages=['ZachsStupidImageLibrary'],
+   install_requires=['Pillow', 'wand']
+)
